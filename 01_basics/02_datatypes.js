003ea71a48769b178dd265becb7ad5bcd041ee50
mayankchaudhary9 <140153812+mayankchaudhary9@users.noreply.github.com>
2023-10-26 06:29:10 +0000

    data types

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all JS codeas newer version
+
+// alert ( 3 + 3 )  // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3)  // code readability should br high
+
+console.log("mayank")     
+
+
+let name = "mayank"
+let age = 20
+let isLoggedIn = false
+let state;
+
+
+// number    => 2 to power 53
+// bigInt
+// string    => ""
+// boolean   => true/false
+// null      => standalone value
+// undefined => 
+// symbol    => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null);      // object
